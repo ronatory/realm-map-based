a83f86709d3f385d5e2d201a7f6919c55cce35a3
ronatory <ronatory@gmail.com>
2016-10-02 09:13:28 +0200

    Update specimen model and add category model

---
+++ Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Agents Partner
+//
+//  Created by ronatory on 01.10.16.
+//  Copyright © 2016 ronatory. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+  dynamic var name = ""
+}
